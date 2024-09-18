0c9915bacc1e88973b829046de3f41fd802df887
Milan De Paepe <milandepaepe@MacBook-Air-van-Milan.local>
2024-09-18 14:56:35 +0200

    start styling in style.css

--- lotTrekking.md
+++ lotTrekking.md
@@ -35,3 +35,15 @@
 
 - button to pick random person or press spacebar
 - box with shows the chosen person with nice animation
+
+
+
+## colors
+
+- #191a19 //black, background
+- #1e5128 //dark green
+- #4e9f3d //green
+- #d8e9a8 //light green ish
+- #f5f5f5 //~white, texst
+- #51a3a3 //  , header
+- #26c485 // , footer
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for
 import re
 from applicant import Applicant
 
@@ -13,7 +13,17 @@ def check_email(email):
     return re.fullmatch(regex, email)
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
+def landing_page():
+    return render_template("index.html")
+
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+
+@app.route("/main", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
         first_name = request.form.get("first_name")
@@ -22,21 +32,21 @@ def home():
         submit = request.form.get("submit", False)
 
         if not first_name:
-            return render_template("index.html",
+            return render_template("main.html",
                                    error="Please enter your first name",
                                    first_name=first_name,
                                    last_name=last_name,
                                    email=email)
 
         if not last_name:
-            return render_template("index.html",
+            return render_template("main.html",
                                    error="Please enter your last name",
                                    first_name=first_name,
                                    last_name=last_name,
                                    email=email)
 
         if not check_email(email):
-            return render_template("index.html",
+            return render_template("main.html",
                                    error="Please enter a valid email",
                                    first_name=first_name,
                                    last_name=last_name,
@@ -45,7 +55,7 @@ def home():
         if submit:
             applications.append(Applicant(first_name, last_name, email))
 
-    return render_template("index.html")
+    return render_template("main.html")
 
 
 @app.route("/trekking")
